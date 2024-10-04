a6e3cec401171c32486bed59458791c5270b9bbe
Gyurcsaklj <gyurcsakl@kkszki.hu>
2024-10-04 12:50:18 +0200

    szamr is ready

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamrendszerek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérlek, adj meg egy tetszőleges tízes számrendszerbeli egész számot: ");
+            int Szam = int.Parse(Console.ReadLine());
+
+            List<int> kettesSzamrendszer = new List<int>();
+
+            if (Szam == 0)
+            {
+                kettesSzamrendszer.Add(0);
+            }
+
+            else
+            {
+                while (Szam > 0)
+                {
+                    int maradek = Szam % 2;
+                    kettesSzamrendszer.Add(maradek);
+                    Szam /= 2;
+                }
+            }
+
+            kettesSzamrendszer.Reverse();
+            Console.Write("Ennek a számnak a kettes számrendszere: ");
+            foreach (int bit in kettesSzamrendszer)
+            {
+                Console.Write(bit);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
